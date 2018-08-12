61c92d20ab2d80ad8a0d8fb7a4a3f6df877546c1
hoangp-mac <hoang.unsw@gmail.com>
2018-08-12 10:38:01 +1000

    tut03 done

--- src/tut03/equals/Point.java
+++ src/tut03/equals/Point.java
@@ -13,24 +13,6 @@ public class Point {
   public boolean equals(Point obj) {
     return (this.x == obj.x) && (this.y == obj.y);
   }
-  
-  @Override
-  public boolean equals(Object obj) {
-    return (this == obj);
-  }
-
-//  @Override
-//  public boolean equals(Object obj) {
-//    if (this == obj) return true;
-//    if (obj == null) return false;
-//    
-//    if (!(obj instanceof Point)) return false;
-//    Point o = (Point) obj;
-//    if (this.x != o.x) return false;
-//    if (this.y != o.y) return false;
-//    
-//    return true;
-//  }
 
   public static void main(String[] args) {
     Point p1 = new Point(1, 2);
@@ -40,6 +22,5 @@ public class Point {
 
     Object p3 = p2;
     System.out.println(p3.equals(p1)); // false. WHY?
-    System.out.println(p1.equals(p3));
   } 
 }
