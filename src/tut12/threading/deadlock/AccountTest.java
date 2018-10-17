25c7eba25d0271f514edac89cd99fc31e2a974f6
Hoang Pham <hoang.unsw@gmail.com>
2018-10-17 17:22:37 +1100

    Update Deadlock Example

--- src/tut12/threading/deadlock/AccountTest.java
+++ src/tut12/threading/deadlock/AccountTest.java
@@ -3,22 +3,22 @@ package tut12.threading.deadlock;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
-    @Test
-    void testTransfer() throws InterruptedException {
-        Account first = new Account();
-        Account second = new Account();
+  @Test
+  void testTransfer() throws InterruptedException {
+    Account first = new Account();
+    Account second = new Account();
 
-        // Create threads.
-        AccountTransferrer firstToSecond = new AccountTransferrer(first, second);
-        AccountTransferrer secondToFirst = new AccountTransferrer(second, first);
+    // Create threads.
+    AccountTransferrer firstToSecond = new AccountTransferrer(first, second, "firstToSecond");
+    AccountTransferrer secondToFirst = new AccountTransferrer(second, first, "secondToFirst");
 
-        Thread t1 = new Thread(firstToSecond);
-        Thread t2 = new Thread(secondToFirst);
+    Thread t1 = new Thread(firstToSecond);
+    Thread t2 = new Thread(secondToFirst);
 
-        t1.start();
-        t2.start();
+    t1.start();
+    t2.start();
 
-        t1.join();
-        t2.join();
-    }
+    t1.join();
+    t2.join();
+  }
 }
--- src/tut12/threading/deadlock/AccountTransferrer.java
+++ src/tut12/threading/deadlock/AccountTransferrer.java
@@ -4,18 +4,21 @@ package tut12.threading.deadlock;
  * Thread which performs account transfers.
  */
 public class AccountTransferrer implements Runnable {
-    private Account from;
-    private Account to;
+  private Account from;
+  private Account to;
+  private String name; // for demonstration
 
-    public AccountTransferrer(Account from, Account to) {
-        this.from = from;
-        this.to = to;
-    }
+  public AccountTransferrer(Account from, Account to, String name) {
+    this.from = from;
+    this.to = to;
+    this.name = name;
+  }
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            from.transfer(to, 1);
-        }
+  @Override
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      from.transfer(to, 1);
+      System.out.println(name + " " + i);
     }
+  }
 }
