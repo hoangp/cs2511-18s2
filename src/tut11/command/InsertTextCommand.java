fbf689462772afe40f9ad63a6c35ca545f1171d9
Hoang Pham <hoang.unsw@gmail.com>
2018-10-17 16:25:40 +1100

    update command patter for tut11

---
+++ src/tut11/command/InsertTextCommand.java
@@ -0,0 +1,25 @@
+package tut11.command;
+
+/**
+ * Concrete command for inserting text.
+ */
+public class InsertTextCommand implements Command {
+  private TextBuffer tb;
+  private char c;
+
+  public InsertTextCommand(TextBuffer tb, char c) {
+    this.tb = tb;
+    this.c = c;
+  }
+
+  @Override
+  public void execute() {
+    tb.insertText(c);
+  }
+
+  @Override
+  public void undo() {
+    System.out.println("Undoing insertion of " + c);
+    tb.deleteText(c);
+  }
+}
---
+++ src/tut11/command/TextBuffer.java
@@ -0,0 +1,15 @@
+package tut11.command;
+
+/**
+ * The Receiver class. In an actual implementation, it would store the lines and
+ * characters in the file.
+ */
+public class TextBuffer {
+  public void insertText(char c) {
+    System.out.println("Inserting " + c);
+  }
+
+  public void deleteText(char c) {
+    System.out.println("Removing " + c);
+  }
+}
